4e29f56c5c2c7d89eab7a342cba1278304912862
Drone <37543774+ishanRox@users.noreply.github.com>
2020-03-22 16:32:16 +0530

    regex added

--- js/pojo.js
+++ js/pojo.js
@@ -2,8 +2,30 @@ function pojoConvert() {
 
     let textArea = document.getElementById("pojo")
     let innerText = textArea.value;
-    let removedImports=innerText.replace(/(package.*)|import.*|[\r\n]+/gm, "");
-    console.log(removedImports);
-    console.log(removedImports.split(";"))
+//finding a class class( )+(.*)( )*{
+    let className = innerText.match(/class( )+(.*)( )*{/)[2];
+    console.log(className);
+    let textInsideClassBody = innerText.match(/(?<=(.|\n|\r)*class.*{)(.|\n)*(?=})/g);
 
+    const textInsideClassBodyMethodsRemoved = textInsideClassBody[0].replace(/{(\n|\r| |.)*?}/g, "");
+
+    const filteredAsLines = (textInsideClassBodyMethodsRemoved.split(/\n/));
+
+    const varialbles = (filteredAsLines.filter(e => e.match(/(?<=(.)+)( )+(\w)*( )*(?=;)/)));
+
+    let typeScriptInterface = "export interface " + className + " {";
+
+    varialbles.map(line => {
+
+        const regExpMatchArray = line.match(/((.)+?) ((\w)*( )*)(?=;)/);
+        let type = regExpMatchArray[1];
+        let varName = regExpMatchArray[3];
+
+        //  type=(type.exec(/(byte|short|int|long|float|double)/)!==null)?"number":dataType;
+        console.log(typeof type);
+        console.log(/(byte|short|int|long|float|double)/.exec(type));
+        typeScriptInterface += (`\n ${type}:${varName};`);
+
+    });
+    console.log(typeScriptInterface + "\n}")
 }
